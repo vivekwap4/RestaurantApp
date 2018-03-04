32e38b0e6cc3a2215f3fa2932e55c646c9ea697f
Vivek Mishra <31122324+vivekwap4@users.noreply.github.com>
2018-03-04 15:46:54 -0600

    Basic Service Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 
 @NgModule({
@@ -26,7 +27,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 	MaterialModule,
 	FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
